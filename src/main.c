dbf170180807faa645a53b9d906787251d29f834
Akim Demaille <akim.demaille@gmail.com>
2018-10-07 08:25:21 +0200

    gnulib: let it use its own PO domain
    See
    https://www.gnu.org/software/gnulib/manual/html_node/Localization.html.
    
    * bootstrap.conf: Create gnulib-po.
    * Makefile.am, configure.ac: Use it.
    * po/POTFILES.in: Remove files now in gnulib.
    * src/main.c: Open the bison-gnulib domain.

--- src/main.c
+++ src/main.c
@@ -66,11 +66,12 @@ main (int argc, char *argv[])
   {
     char *cp = NULL;
     char const *localedir = relocate2 (LOCALEDIR, &cp);
-    (void) bindtextdomain (PACKAGE, localedir);
-    (void) bindtextdomain ("bison-runtime", localedir);
-    free (cp);
+    bindtextdomain ("bison", localedir);
+    textdomain ("bison");
+    bindtextdomain ("bison-gnulib", localedir);
+    bindtextdomain ("bison-runtime", localedir);
+    //    free (cp);
   }
-  (void) textdomain (PACKAGE);
 
   {
     char const *cp = getenv ("LC_CTYPE");
